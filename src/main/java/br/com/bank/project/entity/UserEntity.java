af163f05b83582789bbc0072a10966fe8085dc18
Rodrigo Ruiz <rodrigo.c.ruiz@gmail.com>
2026-06-18 17:59:51 -0300

    Update UserEntity and add UserDTO with login field (#6)
    * UsuarioEntity update.
    
    * UsuarioEntity update. (#2)
    
    Update ok.
    
    * Remove application.properties from repository
    
    * Update UserEntity
    
    * Add UserDTO and update UserEntity with login field
    
    * Add UserDTO and update UserEntity with login field

---
+++ src/main/java/br/com/bank/project/entity/UserEntity.java
@@ -0,0 +1,88 @@
+package br.com.bank.project.entity;
+
+import br.com.bank.project.dto.UserDTO;
+import jakarta.persistence.*;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "PRJ_USERS")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+
+    public UserEntity (UserDTO userDTO) {
+        BeanUtils.copyProperties(userDTO, this);
+    }
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin(){ return login;}
+
+    public void setLogin(String login){ this.login = login;}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserEntity other = (UserEntity) obj;
+        return Objects.equals(id, other.id);
+    }
+}
