40d6331a7763667f8cacef4a323dac21003fd4e8
Rodrigo Ruiz <rodrigo.c.ruiz@gmail.com>
2026-08-18 14:51:11 -0300

    Stop tracking

--- bank/project/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bank.project.repository;
-
-import br.com.bank.project.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
